95acfc92a8d0a3a2832291b97842d439c84ff1d5
Fabio Ayres <fabioamayres@gmail.com>
2023-01-09 22:32:32 -0300

    Add solution for exercise 1.32

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,19 +1,22 @@
 # pcost.py
+import csv
 def portfolio_cost(filename):
     stock_sum = 0
     with open(filename, 'rt') as f:
-        next(f)
-        for line in f:
-            row = line.split(',')
+        lines = csv.reader(f)
+        next(lines)
+        for line in lines:
+            print(line)
             try:
-                shares = int(row[1])
-                price = float(row[2])
+                shares = int(line[1])
+                price = float(line[2])
                 stock_sum += shares*price
             except ValueError:
                 print('Missing value!', line)
         
         return stock_sum
-    
+
+portfolio_cost('Data/portfolio.csv')    
 cost = portfolio_cost('Data/missing.csv')
 print('Total cost:', cost)
 # Exercise 1.27
