c9f8da7769531139728a453663fa8a5dbe940c58
Fabio Ayres <fabioamayres@gmail.com>
2023-01-09 21:57:55 -0300

    Add solution for exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,11 @@
 # pcost.py
-#
+stock_sum = 0
+with open('Data/portfolio.csv', 'rt') as f:
+    next(f)
+    for line in f:
+        row = line.split(',')
+        stock_sum += int(row[1])*float(row[2])
+    
+    print(f'Total cost: {stock_sum}')
+
 # Exercise 1.27
