9b904ab2e69b9031e9b4a1de07e4560b71ff2abe
Fabio Ayres <fabioamayres@gmail.com>
2023-01-10 08:09:00 -0300

    Add solution for exercise 2.6

--- Work/report.py
+++ Work/report.py
@@ -1,5 +1,23 @@
 # report.py
 import csv
+from pprint import pprint
+def read_prices(filename):
+     portfolio = {}
+     with open(filename, 'rt') as f:
+        lines = csv.reader(f)
+        next(f)
+        for line in lines:
+            try:   
+                portfolio[line[0]] = float(line[1])
+            except IndexError:
+                print('Value missing!')
+            print(line)
+        
+        return portfolio
+
+
+
+
 def read_portfolio(filename):
     portfolio = []
     with open(filename, 'rt') as f:
