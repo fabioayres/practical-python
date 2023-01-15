39353227aeaa1abb9429268fe1d6e60228e44f53
Fabio Ayres <fabioamayres@gmail.com>
2023-01-15 13:35:59 -0300

    Add solution for exercise 2.16

--- Work/report.py
+++ Work/report.py
@@ -22,12 +22,13 @@ def read_portfolio(filename):
     portfolio = []
     with open(filename, 'rt') as f:
         lines = csv.reader(f)
-        next(lines)
+        headers = next(lines)
         for line in lines:
+            record = dict(zip(headers,line))
             holding={
-                'name': line[0],
-                'share' : int(line[1]),
-                'price' : float(line[2])
+                'name': record['name'],
+                'share' : int(record['shares']),
+                'price' : float(record['price'])
             }
             portfolio.append(holding)
             
